3344f6f9cf35bf906f26d42352dad81db2bdb924
kaayoung <2catailna@naver.com>
2024-04-24 23:03:06 +0900

    feat : chatroomheader 메뉴 모달 publish

--- src/pages/chat-room/components/ChatRoomHeader.tsx
+++ src/pages/chat-room/components/ChatRoomHeader.tsx
@@ -1,9 +1,42 @@
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
+type ChatMenuType = "exit" | "invite" | "accuse";
+
+const chatMenuList: { type: ChatMenuType; name: string }[] = [
+  { type: "exit", name: "채팅방 나가기" },
+  { type: "invite", name: "모임에 초대하기" },
+  // {type : 'accuse' , name : '신고하기'},
+];
+
 export default function ChatRoomHeader() {
+  const menuRef = useRef<HTMLUListElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
+
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(e.target as Node) &&
+      !buttonRef.current?.contains(e.target as Node)
+    ) {
+      setIsMenuOpened(false);
+    }
+  };
+
+  const handleMenuButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsMenuOpened(!isMenuOpened);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
   return (
     <Container>
       <LeftWrapper>
@@ -13,12 +46,16 @@ export default function ChatRoomHeader() {
           <UserName>유저닉네임</UserName>
         </TitleWrapper>
       </LeftWrapper>
-      <MenuBtn>
+      <MenuBtn onClick={handleMenuButtonClick} ref={buttonRef}>
         <FontAwesomeIcon icon={faEllipsisVertical} size="lg" />
-        <MenuModal>
-          <MenuItem>hii</MenuItem>
-        </MenuModal>
       </MenuBtn>
+      {isMenuOpened && (
+        <MenuModal ref={menuRef} id="menu-modal">
+          {chatMenuList.map((menu, i) => (
+            <MenuItem key={`menu-${i}-${menu.type}`}>{menu.name}</MenuItem>
+          ))}
+        </MenuModal>
+      )}
     </Container>
   );
 }
@@ -62,17 +99,22 @@ const MenuBtn = styled.button``;
 
 const MenuModal = styled.ul`
   position: absolute;
+  top: 3rem;
+  right: 1.5rem;
   background-color: white;
   border: 1px solid var(--purple);
   border-radius: 4px;
   padding: 1rem;
   display: flex;
   flex-direction: column;
-  gap: 0.2rem;
+  gap: 0.5rem;
 `;
 
 const MenuItem = styled.li`
   cursor: pointer;
   font-size: 0.8rem;
   font-weight: 500;
+  &:hover {
+    color: var(--purple);
+  }
 `;
